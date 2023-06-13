d64ee88d6d27b41621fd7ed7c2dc8c5de440d463
bigMouse <2388505323@qq.com>
2023-06-13 23:23:42 +0800

    计时器完成

--- MineSweeper/mainwindow.cpp
+++ MineSweeper/mainwindow.cpp
@@ -7,13 +7,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+//    QFont ft;
+//    ft.setPointSize(18);
+//    ft.setBold(true);
+//    ui->label_h->setFont(ft);
+//    ui->label_m->setFont(ft);
+//    ui->label_s->setFont(ft);
+//    ui->label_h->setAlignment(Qt::AlignCenter);
+//    ui->label_m->setAlignment(Qt::AlignCenter);
+//    ui->label_s->setAlignment(Qt::AlignCenter);
+    updataCount();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
 
 
 //退出功能
@@ -31,4 +40,115 @@ void MainWindow::closeEvent(QCloseEvent *event){
         event->accept();    //确认退出
     else
         event->ignore();    //拒绝退出
+}
+
+//计时器
+
+void MainWindow::updataCount(){
+
+    connect(timer_up, &QTimer::timeout, [=](){
+        count_s++;
+        calculateCount();
+    });
+}
+
+void MainWindow::calculateCount(){
+
+    if(count_s<10)
+        {
+            ui->label_s->setText("0" + QString::number(count_s));
+        }
+        else if(count_s>=10 && count_s<60)
+        {
+            ui->label_s->setText(QString::number(count_s));
+        }
+        else
+        {
+            count_s=0;
+            ui->label_s->setText("00");
+            count_m++;
+            if(count_m<10)
+            {
+                ui->label_m->setText("0" + QString::number(count_m));
+            }
+            else if(count_m>=10 && count_m<60)
+            {
+                ui->label_m->setText(QString::number(count_m));
+            }
+            else
+            {
+                count_m=0;
+                ui->label_m->setText("00");
+                count_h++;
+                if(count_h<10)
+                {
+                    ui->label_h->setText("0" + QString::number(count_h));
+                }
+                else
+                {
+                    ui->label_h->setText(QString::number(count_h));
+                }
+            }
+        }
+}
+
+
+void MainWindow::startCount(){
+
+    if(workingCount == false){
+        timer_up->start(1000);
+        workingCount = true;
+    }
+}
+
+void MainWindow::stopCount(){
+
+    if(workingCount ==true){
+        timer_up->stop();
+        workingCount = false;
+    }
+}
+
+void MainWindow::resetCount(){
+
+    timer_up->stop();
+    workingCount = false;
+    ui->label_s->setText("00");
+    ui->label_m->setText("00");
+    ui->label_h->setText("00");
+    count_s=0;
+    count_m=0;
+    count_h=0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+void MainWindow::on_pushButtton_start_clicked()
+{
+    if(workingCount ==false){
+        startCount();
+        workingCount = true;
+        ui->pushButtton_start->setText("暂停");
+    }
+    else{
+        stopCount();
+        workingCount = false;
+         ui->pushButtton_start->setText("开始");
+
+    }
+}
+
+void MainWindow::on_pushButton_reset_clicked()
+{
+    resetCount();
+    ui->pushButtton_start->setText("开始");
 }
--- MineSweeper/mainwindow.h
+++ MineSweeper/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QMessageBox>
 #include <QCloseEvent>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,10 +18,39 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+
+    //退出函数
     void closeEvent(QCloseEvent *event);
+
+    QTimer *timer_up = new QTimer(this);
+    //计时器时间
+    int count_h =0;
+    int count_m =0;
+    int count_s =0;
+    //计数器工作状态
+     bool workingCount = false;
+    //计时器开始函数
+    void startCount();
+    //计时器暂停函数
+    void stopCount();
+    //计时器重置函数
+    void resetCount();
+    //计时器更新函数
+    void updataCount();
+    //时间计算
+    void calculateCount();
+
+
 
 private slots:
     void on_exit_triggered();
+
+
+
+    void on_pushButtton_start_clicked();
+
+    void on_pushButton_reset_clicked();
 
 private:
     Ui::MainWindow *ui;
